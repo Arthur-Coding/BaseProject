36003f0411da25301a48f5b844f391aaf963214b
ArthurShuai <zhixingui_liushuai@163com>
2017-03-02 16:19:11 +0800

    基础项目框架搭建 上传更新  2017/3/2 16:19

---
+++ BaseProject/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  BaseProject
+//
+//  Created by ArthurShuai on 2017/3/2.
+//  Copyright © 2017年 qusu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ BaseProject/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  BaseProject
+//
+//  Created by ArthurShuai on 2017/3/2.
+//  Copyright © 2017年 qusu. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
